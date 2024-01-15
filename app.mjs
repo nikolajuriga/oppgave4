25c3d50497492ec8c020927edbe2818c34d2e675
nikolajuriga <156321940+nikolajuriga@users.noreply.github.com>
2024-01-15 13:53:53 +0100

    first upload

---
+++ app.mjs
@@ -0,0 +1,7 @@
+import chalk from "chalk";
+const log = console.log;
+
+log(chalk.blue("Why"), chalk.red("did"), chalk.magenta("the"), chalk.blueBright("bicycle"), chalk.green("go"),chalk.blue("to"), chalk.red("the"), chalk.magenta("doctor?"));
+
+log(chalk.yellow("because"), chalk.black("it"), chalk.cyan("was"), chalk.blue("two-tired!"));
+
